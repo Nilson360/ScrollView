5db2b245aa2afbdd117097d97b4fbd80ab436682
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2022-03-25 11:40:24 +0100

    Initial Commit

---
+++ Shared/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  Shared
+//
+//  Created by Noslin on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
